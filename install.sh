5eab55e1bf5ac642d4984382456e744f70d5eca5
Amir-jpg-png <mrelmesiry@gmail.com>
2025-08-09 20:39:39 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+set -e 
+REPO_URL="http://localhost:3002/Amir/ToDo-CLI.git"
+CLONE_DIR="/tmp/todo-install"
+
+if [-d "$CLONE_DIR/.git"]; then
+  echo "Updating existing repository"
+  git -C "$CLONE_DIR" pull
+else
+  echo "Cloning repository..."
+  git clone "$REPO_URL" "$CLONE_DIR"
+fi
+
+chmod +x "$CLONE_DIR/todo.py"
+sudo cp "$CLONE_DIR/todo.py" "/usr/local/bin/todo"
+
+echo "Installed todo script! You can now run 'todo' from Anywhere!"
